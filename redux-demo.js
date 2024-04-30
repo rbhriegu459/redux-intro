f9237e30983bdfe0c4f1448b3e4b5255fe344e8e
rbhriegu459 <rbhriegu459@gmail.com>
2024-04-30 22:17:38 +0530

    incrementBy1

--- redux-demo.js
+++ redux-demo.js
@@ -1,3 +1,18 @@
-const redux = require("redux");
+const redux = require('redux');
 
-const store  = redux.createStore();
+const counterReducer = (state = {counter : 0 }, action) => {
+    return {
+        counter: state.counter + 1
+    };
+} ;
+
+const store  = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+    const latetstState= store.getState();
+    console.log(latetstState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({type: 'increment'});
